94cb805cbe583c0643aa7a70dbfa12b472b9537c
mr-metschnikowia <gorbunov.lev@googlemail.com>
2022-09-23 23:19:45 +0100

    MySQL integration

--- api.php
+++ api.php
@@ -3,20 +3,144 @@
 
 <?php
 
-$database = array("24hCardiovascularN8"=>array("crouch end cardio 1", "crouch end cardio 2"), "24hCardiovascularN10"=>array("umpaland cardio 1"));
-// consturct rudimentary database
+$response = array();
+// query result 
+
+$username = "root";
+
+$password = "";
+
+$dbname = "nhs";
+
+$servername = "localhost";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// construct MySQL database connection 
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+// check database connection 
+
+$tempDob = $tempPost = $tempTime = $tempSg = $tempGend = "";
+// define interim variables from from parameters, used before form is completed
 
 $dob = $postcode = $timeFrame = $sgGroup = $gender = "";
-// define variables obtained from requests
-$dobError = $postError = $timeError = "";
+// define final variables, set once form is complete
+
+$dobError = $postError = $timeError = $genderError = "";
 // define errors 
 
-function validateParameters($parameter) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//check if post request is made 
+	$lock = 0;
+	if (empty($_POST['dob'])) {
+		$dobError = "DOB is required!";
+	} else {
+		$tempDob = validateParameter($_POST["dob"]);
+		// general parameter validation 
+	}  
+	if (empty($_POST['postcode'])) {
+		$postError = "Postcode is required!";
+	} else {
+		$tempPost = validateParameter($_POST["postcode"]);
+	}
+	if (empty($_POST['gender'])) {
+		$genderError = "Gender is required!";
+	} else {
+		$tempGend = validateParameter($_POST["gender"]);
+		$lock += 1;
+	}
+	if (empty($_POST['timeFrame'])) {
+		$timeError = "Timeframe is required!";
+	} else {
+		$tempTime = validateParameter($_POST["timeFrame"]);
+	}
+	$tempSg = validateParameter($_POST["sgGroup"]);
+	// set temp variables - before number of required variables completed is calculated
+
+	if (preg_match("*^(0[1-9]|[1-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/[0-9]{4}$*", $tempDob)) {
+	//if (!preg_match("*^[0-9]{4}/(0[1-9]|1[0-2])/(0[1-9]|[1-2][0-9]|3[0-1])$*",$date)) {
+		$lock += 1;
+	} else {
+		$dobError = "Invalid format!";
+	}
+	if (!preg_match("/^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$/", $tempPost)) {
+	    $postError = "Invalid format!";
+	} else {
+		$lock += 1;
+	}
+	if (!preg_match("/^([0-9]){1,2}$/",$tempTime)) {
+	    $timeError = "Invalid format!";
+	} else {
+		$lock += 1;
+	}
+	//specific validation - DOB, postcode, timeframe -> if not exit script -> if yes write output 
+
+	if ($lock == 4) {
+
+		$stmt = $conn->prepare("SELECT serviceName FROM services WHERE timeFrame=? AND postcode=? and sgGroup=?");
+		// create sql prepared statement
+
+		$stmt->bind_param("iss", $timeFrame, $postcode, $sgGroup);
+		// bind variables to prepared statement
+
+		$dob = $tempDob;
+		$postcode = substr($tempPost, 0, 2);
+		$gender = $tempGend;
+		$timeFrame = (int)$tempTime;
+		$sgGroup = $tempSg;
+		// set final query parameters
+
+		$stmt->execute();
+		// execute prepared statement
+
+		$result = $stmt->get_result();
+		// get query result 
+
+		while ($row = $result->fetch_assoc()) {
+			array_push($GLOBALS["response"], $row['serviceName']);
+		}
+		// get specific column from rows of result 
+
+		$stmt->close();
+		$conn->close();
+		// close database connection and prepared statement 
+	}
+}
+
+/*
+function specificValidation ($regex, $cleanParameter, $error, $finalParameter) {
+	if (!preg_match($regex, $cleanParameter)) {
+	    $error = "Invalid format!";
+		exit()
+	} else {
+		$finalParameter = $cleanParameter;
+	}
+}
+// specific regex validation function
+*/
+
+function validateParameter($parameter) {
 	$parameter = trim($parameter);
 	$parameter = stripslashes($parameter);
 	$parameter = htmlspecialchars($parameter);
 	return $parameter;
 }
+// General validation function 
+
+/*
+function queryDatabase($query, $database) {
+	global $result;
+	foreach ($database as $key=>$value) {
+		if ($key == $query) {
+			$result = $value;
+			break;
+		}
+	}
+}
+// simple database query function 
+*/
 
 ?>
 
--- ui.php
+++ ui.php
@@ -11,31 +11,43 @@
 <h2>Make a query:</h2><br /><br />
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-DOB: <input type="text" name="dob" value="<?php echo $dob;?>">
+DOB: <input type="text" name="dob" value="<?php echo $tempDob;?>">
 <span class="error"> * <?php echo $dobError;?></span>
 <br /><br />
-Postcode: <input type="text" name="postcode" value="<?php echo $postcode;?>">
+Postcode: <input type="text" name="postcode" value="<?php echo $tempPost;?>">
 <span class="error"> * <?php echo $postError;?></span>
 <br /><br />
-Time frame: <input type="text" name="timeFrame" value="<?php echo $timeFrame;?>">
+Time frame: <input type="text" name="timeFrame" value="<?php echo $tempTime;?>">
 <span class="error"> * <?php echo $timeError;?></span>
 <br /><br />
 
 Symptom group:
 <select name="sgGroup">
-<option value="Cardiovascular" <?php if ($sgGroup == "Cardiovascular") echo "selected"; ?>>Cardiovascular</option>
-<option value="GI" <?php if ($sgGroup == "GI") echo "selected"; ?>>GI</option>
-<option value="Oncology" <?php if ($sgGroup == "Oncology") echo "selected"; ?>>Oncology</option>
-<option value="A&E" <?php if ($sgGroup == "A&E") echo "selected"; ?>>A&E</option>
+<option value="Cardiovascular" <?php if ($tempSg == "Cardiovascular") echo "selected"; ?>>Cardiovascular</option>
+<option value="GI" <?php if ($tempSg == "GI") echo "selected"; ?>>GI</option>
+<option value="Oncology" <?php if ($tempSg == "Oncology") echo "selected"; ?>>Oncology</option>
+<option value="A&E" <?php if ($tempSg == "A&E") echo "selected"; ?>>A&E</option>
 </select><br /><br />
 
 Gender:
-<input type="radio" <?php if (isset($gender) && $gender == "Male") echo "checked"; ?> name="gender">Male
-<input type="radio" <?php if (isset($gender) && $gender == "Female") echo "checked"; ?> name="gender">Female
-<input type="radio" <?php if (isset($gender) && $gender == "Other") echo "checked"; ?> name="gender">Other<br /><br /><br /><br />
+<input type="radio" <?php if (isset($tempGend) && $tempGend == "Male") { echo "checked"; } ?> name="gender" value="Male">Male
+<input type="radio" <?php if (isset($tempGend) && $tempGend == "Female") { echo "checked"; } ?> name="gender" value="Female">Female
+<input type="radio" <?php if (isset($tempGend) && $tempGend == "Other") { echo "checked"; } ?> name="gender"  value="Other">Other
+<span class="error"> * <?php echo $genderError;?></span>
+<br /><br /><br /><br />
 <input type="submit"><br />
 </form>
 <!-- UI -->
+
+<br /><br />
+<h2>Best Services:</h2> 
+<br />
+
+<?php
+
+foreach ($response as $service) { echo $service . "<br />"; }
+// get best service based on database search
+?>
 
 </body>
 </html>
